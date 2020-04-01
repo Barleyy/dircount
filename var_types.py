711878c53a632ee5925910e57b23e5c903d2074d
Paweł Gałka <p.k.galka@gmail.com>
2020-04-01 22:35:46 +0200

    added float type

--- var_types.py
+++ var_types.py
@@ -1,6 +1,8 @@
 from enum import Enum
 from directory_functions import *
 import settings
+import math
+
 
 class Types(Enum):
     _int = 1
@@ -25,7 +27,7 @@ def declare_int(bits_dir, name_directory):
             f"First subdirectory of directory {bits_dir.parent_path.path} of type 'declare int' must have 16 subdirectories but has {bits_dir.dirlen()}")
     value = parse_integer_value(bits_dir, True)
     var_name = parse_string_value(name_directory)
-    attach_variable(var_name,value)
+    attach_variable(var_name, value)
     print(f"VAR TYPE INT {var_name} = {value}")
 
 
@@ -33,7 +35,16 @@ def declare_float(value_dir, name_directory):
     if value_dir.dirlen() != 32:
         raise ValueError(
             f"First subdirectory of directory {value_dir.parent_path.path} of type 'declare float' must have 32 subdirectories but has {value_dir.dirlen()}")
-    print("declaring float")
+    mantissa_values = [2 ** -i for i in range(1,24)]
+    exponent_values = [2 ** i for i in range(7,-1,-1)]
+    bits = list(map(Directory.directory_to_bit, value_dir.get_children_paths()))
+    exponent = sum([b * e for b, e in zip(exponent_values, bits[1:8])]) - 127
+    mantissa = 1 + sum([b * e for b, e in zip(mantissa_values, bits[9:])])
+    sign = (1, -1)[bits[0]]
+    var_name = parse_string_value(name_directory)
+    value = sign*mantissa*math.pow(2,exponent)
+    attach_variable(var_name,value)
+    print(f"VAR TYPE FLOAT {var_name} = {value}")
 
 
 def declare_char(char_dir, name_directory):
@@ -43,14 +54,14 @@ def declare_char(char_dir, name_directory):
             f"ASCII (0-127) but has {char_dir.dirlen()}")
     char = chr(parse_integer_value(char_dir, False))
     var_name = parse_string_value(name_directory)
-    attach_variable(var_name,char)
+    attach_variable(var_name, char)
     print(f"VAR TYPE CHAR {var_name} = {char}")
 
 
 def declare_string(chars_dir, name_directory):
     string = parse_string_value(chars_dir)
     var_name = parse_string_value(name_directory)
-    attach_variable(var_name,string)
+    attach_variable(var_name, string)
     print(f"VAR TYPE STRING {var_name} = {string}")
 
 
@@ -71,15 +82,16 @@ def parse_integer_value(data_dir, is_number):
     bit_values = [2 ** i for i in range(range_value)]
     bits = list(map(Directory.directory_to_bit, data_dir.get_children_paths()))
     res = [b * e for b, e in zip(bit_values, bits[start_index:])]
-    sign =(1,-1)[is_number and bits[0]]
-    return sign*sum(res)
+    sign = (1, -1)[is_number and bits[0]]
+    return sign * sum(res)
 
 
-def attach_variable(name,value):
+def attach_variable(name, value):
     if name in settings.variables:
         raise ValueError(f"Variable {name} already defined to {settings.variables[name]}")
     else:
         settings.variables[name] = value
+
 
 types_dict = {
     Types._int: declare_int,
