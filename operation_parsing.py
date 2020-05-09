d9663e4c527da9b5e3ed7e8c8cc26c50a9ba7ece
Paweł Gałka <p.k.galka@gmail.com>
2020-05-09 09:43:06 +0200

    new todo

--- operation_parsing.py
+++ operation_parsing.py
@@ -8,8 +8,10 @@ def parse_operation_argument(operation_arg_dir):
     is_link_val = operation_arg_dir.is_var_linked()
 
     if is_link_val:  # is link
+        # TODO: change operation_arg_dir.navigate_to_nth_child(0).get_link_path() to new link parses function
         invoked_function = settings.get_currently_invoked_function()
-        arg_val = invoked_function.variable_stack.get_var_by_path(operation_arg_dir.navigate_to_nth_child(0).get_link_path()).value
+        arg_val = invoked_function.variable_stack.get_var_by_path(operation_arg_dir.navigate_to_nth_child(0)
+                                                                  .get_link_path()).value
 
     elif operation_arg_dir.dirlen() != 3:  # not link and not operation -> simple type
         _type = value_parsing.len_types[operation_arg_dir.dirlen()]
